1eb0947dccd33e9b38aac22d3fa3349c3bebc6d4
sarath <sarathdon9245@gmail.com>
2021-09-07 18:26:09 +0530

    🚀 add - product _container dom

--- src/js/index.js
+++ src/js/index.js
@@ -2,84 +2,104 @@ async function fetchAllImagesInfo() {
 	const mockyURl =
 		'http://run.mocky.io/v3/6f7a76ed-d6f5-4b54-be23-bf9a141c982a';
 
-	// fetch(mockyURl)
-	// 	.then((response) => response.json())
-	// 	.then((data) => {
-	// 		console.log(data);
-	// 	}).catch(err => console.log(err));
-
-	// try {
-	// 	const data = (async () => {
-	// 		const mockyRes = await fetch(mockyURl);
-	// 		const data = await mockyRes.json();
-	// 		return data;
-	// 		console.log(data);
-	// 	});
-	// }
-	// catch (err) {
-	// 	alert('Error while fetching the images info', err);
-	// }
-
 	const mockyData = await fetch(mockyURl)
 		.then((res) => res.json())
-		.then((data) => {
+		.then(async (data1) => {
+			let imagesUrl = [];
+			let imagePosition = 0;
+
+			for (let { image } of data) {
+				imagesUrl.push(image);
+			}
+
+			try {
+				let actualImagesUrl = await Promise.all(
+					imagesUrl.map(async (url) => {
+						const res = await fetch(url);
+						const data = await res.url;
+						// console.log(actualImagesUrl);
+						for (let imageData of data1) {
+							console.log(imageData);
+							imageData.image = actualImagesUrl[imagePosition];
+							imagePosition += 1;
+						}
+						console.log(data1);
+						// const productGridContainer = $('.product_grid_container');
+						for (let i = 0; i < data1.length; i++) {
+							const div = document.createElement('div');
+							console.log('cpc');
+							const img = document.createElement('img');
+							const p1 = document.createElement('p');
+							const p2 = document.createElement('p');
+							div.className = 'product_container';
+							img.src = data1[i].image;
+							img.attributes.alt = data1[i].name;
+							img.className = 'product_image';
+							p1.className = 'product_text';
+							p2.className = 'product_description';
+							div.appendChild(img);
+							div.appendChild(p1);
+							div.appendChild(p2);
+							console.log(div);
+							$('.product_grid_container').appendChild(div);
+						}
+						// createProductContainersAndAdd(data);
+						return data1;
+						// return data;
+					})
+				);
+			} catch (error) {
+				alert('Error while fetching the actualImagesUrl', error);
+			}
 			return data;
 		})
 		.catch((err) => {
 			alert('Error while fetching the images info', err);
 		});
-
-	let imagesUrl = [];
-	let imagePosition = 0;
-
-	for (let { image } of mockyData) {
-		imagesUrl.push(image);
-	}
-
-	try {
-		let actualImagesUrl = await Promise.all(
-			imagesUrl.map(async (url) => {
-				const res = await fetch(url);
-				const data = await res.url;
-				return data;
-			})
-		);
-		console.log(actualImagesUrl);
-		for (let imageData of mockyData) {
-			console.log(imageData);
-			imageData.image = actualImagesUrl[imagePosition];
-			imagePosition += 1;
-		}
-		console.log(mockyData);
-		return mockyData;
-	} catch (error) {
-		alert('Error while fetching the actualImagesUrl', err);
-	}
 }
 
 const imagesInfo = fetchAllImagesInfo();
 
 $('.product_table_container').hide();
 
-// if (localStorage.getItem('currentItem') !== 'table') {
-// }
-
 let currentView = 'grid';
-// localStorage.setItem('currentView', currentView);
 
-// if (
-// 	localStorage.getItem('currentView') === 'grid' ||
-// 	!localStorage.getItem('currentView')
-// ) {
-// 	currentView === 'grid';
-// } else {
-// 	currentView = 'table';
+{
+	/* <div class="product_container">
+	<img class="product_image" src="../flower.jpg" alt="Flower" />
+	<p class="product_text">Flower</p>
+	<p class="product_description">
+		Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil ad eos
+		labore.
+	</p>
+</div>; */
+}
+
+// implement dynamic product_container
+
+// function createProductContainersAndAdd(imagesInfo) {
+// 	const productGridContainer = $('.product_grid_container');
+// 	for (let i = 0; i < imagesInfo.length; i++) {
+// 		const div = document.createElement('div');
+// 		console.log('cpc');
+// 		const img = document.createElement('img');
+// 		const p1 = document.createElement('p');
+// 		const p2 = document.createElement('p');
+// 		div.className = 'product_container';
+// 		img.src = imagesInfo[i].image;
+// 		img.attributes.alt = imagesInfo[i].name;
+// 		img.className = 'product_image';
+// 		p1.className = 'product_text';
+// 		p2.className = 'product_description';
+// 		div.appendChild(img);
+// 		div.appendChild(p1);
+// 		div.appendChild(p2);
+// 		console.log(div);
+// 		$('.product_grid_container').appendChild(div);
+// 	}
 // }
-
-// let currentView = localStorage.getItem('currentView');
 
 // implement toggler - grid / table view
-
 
 $('.grid_view_option').click(function (e) {
 	e.preventDefault();
@@ -93,8 +113,6 @@ $('.grid_view_option').click(function (e) {
 $('.table_view_option').click(function (e) {
 	e.preventDefault();
 	currentView = 'table';
-	// localStorage.setItem('currentView', currentView);
-	// currentView = localStorage.setItem('currentView', 'table');
 	$('.table_view_option').css('text-decoration', 'underline');
 	$('.grid_view_option').css('text-decoration', 'none');
 	$('.product_grid_container').hide();
@@ -145,6 +163,8 @@ $('.search_input').on('input', function () {
 		}
 	}
 });
+
+// table sorter configurations
 
 $(function () {
 	$('table').tablesorter({
