aaf07900487e991967c0269bffded33a1cd3d89c
sarath <sarathdon9245@gmail.com>
2021-09-08 10:35:26 +0530

    🚀 feat - add loader, table sorter with dynamic data

--- src/js/index.js
+++ src/js/index.js
@@ -1,89 +1,80 @@
-async function fetchAllImagesInfo() {
-	const mockyURl =
-		'http://run.mocky.io/v3/6f7a76ed-d6f5-4b54-be23-bf9a141c982a';
+const mockyURl = 'http://run.mocky.io/v3/6f7a76ed-d6f5-4b54-be23-bf9a141c982a';
+let inGridView = true;
 
-	// fetch(mockyURl)
-	// 	.then((response) => response.json())
-	// 	.then((data) => {
-	// 		console.log(data);
-	// 	}).catch(err => console.log(err));
-
-	// try {
-	// 	const data = (async () => {
-	// 		const mockyRes = await fetch(mockyURl);
-	// 		const data = await mockyRes.json();
-	// 		return data;
-	// 		console.log(data);
-	// 	});
-	// }
-	// catch (err) {
-	// 	alert('Error while fetching the images info', err);
-	// }
-
-	const mockyData = await fetch(mockyURl)
-		.then((res) => res.json())
-		.then((data) => {
-			return data;
-		})
-		.catch((err) => {
-			alert('Error while fetching the images info', err);
-		});
-
-	let imagesUrl = [];
-	let imagePosition = 0;
-
-	for (let { image } of mockyData) {
-		imagesUrl.push(image);
-	}
-
-	try {
-		let actualImagesUrl = await Promise.all(
-			imagesUrl.map(async (url) => {
-				const res = await fetch(url);
-				const data = await res.url;
-				return data;
+function fetchAllImagesInfo() {
+	const productGridContainer = $('.product_grid_container');
+	const loaderImg = document.createElement('img');
+	loaderImg.src = '../src/Hourglass.gif';
+	productGridContainer.append(loaderImg);
+	return new Promise((resolve, reject) => {
+		fetch(mockyURl)
+			.then((res) => res.json())
+			.then((data) => {
+				loaderImg.remove();
+				createProductContainersAndAdd(data);
+				resolve(data);
 			})
-		);
-		console.log(actualImagesUrl);
-		for (let imageData of mockyData) {
-			console.log(imageData);
-			imageData.image = actualImagesUrl[imagePosition];
-			imagePosition += 1;
-		}
-		console.log(mockyData);
-		return mockyData;
-	} catch (error) {
-		alert('Error while fetching the actualImagesUrl', err);
+			.catch((err) => {
+				console.error(('Error while fetching the images info', err));
+			});
+	});
+}
+
+fetchAllImagesInfo(mockyURl);
+
+function createProductContainersAndAdd(data) {
+	const productGridContainer = $('.product_grid_container');
+	for (let i = 0; i < data.length; i++) {
+		const productDiv = document.createElement('div');
+		const productImg = document.createElement('img');
+		const productText = document.createElement('p');
+		const productDescription = document.createElement('p');
+		productDiv.className = 'product_container';
+		productImg.src = data[i].image;
+		productImg.attributes.alt = data[i].name;
+		productImg.className = 'product_image';
+		productText.className = 'product_text';
+		productDescription.className = 'product_description';
+		productText.innerText = data[i].name;
+		productDescription.innerText = data[i].description;
+		productDiv.append(productImg);
+		productDiv.append(productText);
+		productDiv.append(productDescription);
+		productGridContainer.append(productDiv);
 	}
 }
 
-const imagesInfo = fetchAllImagesInfo();
+function createTableProductRowAndAdd(data) {
+	const productTableContainer = $('table tbody');
+	for (let i = 0; i < data.length; i++) {
+		const productRow = document.createElement('tr');
+		const productImage = document.createElement('img');
+		const productTdForImage = document.createElement('td');
+		const productName = document.createElement('td');
+		const productDescritpion = document.createElement('td');
+		productRow.className = 'product_row';
+		productImage.src = data[i].image;
+		productImage.className = 'product_table_image';
+		productImage.attributes.alt = data[i].image;
+		productTdForImage.append(productImage);
+		productName.className = 'product_name';
+		productName.innerText = data[i].name;
+		productDescritpion.className = 'product_table_description';
+		productDescritpion.innerText = data[i].description;
+		productRow.append(productTdForImage);
+		productRow.append(productName);
+		productRow.append(productDescritpion);
+		productTableContainer.append(productRow);
+	}
+}
 
 $('.product_table_container').hide();
 
-// if (localStorage.getItem('currentItem') !== 'table') {
-// }
-
-let currentView = 'grid';
-// localStorage.setItem('currentView', currentView);
-
-// if (
-// 	localStorage.getItem('currentView') === 'grid' ||
-// 	!localStorage.getItem('currentView')
-// ) {
-// 	currentView === 'grid';
-// } else {
-// 	currentView = 'table';
-// }
-
-// let currentView = localStorage.getItem('currentView');
-
-// implement toggler - grid / table view
-
+// Toggler - grid / table view
 
 $('.grid_view_option').click(function (e) {
 	e.preventDefault();
-	currentView = 'grid';
+	inGridView = true;
 	$('.grid_view_option').css('text-decoration', 'underline');
 	$('.table_view_option').css('text-decoration', 'none');
 	$('.product_table_container').hide();
@@ -92,18 +83,21 @@ $('.grid_view_option').click(function (e) {
 
 $('.table_view_option').click(function (e) {
 	e.preventDefault();
-	currentView = 'table';
-	// localStorage.setItem('currentView', currentView);
-	// currentView = localStorage.setItem('currentView', 'table');
+	fetchAllImagesInfo(mockyURl)
+		.then((data) => {
+			createTableProductRowAndAdd(data);
+			$('table.product_table_container').trigger('update');
+		})
+		.catch((err) => {
+			alert(err);
+		});
+
+	inGridView = false;
 	$('.table_view_option').css('text-decoration', 'underline');
 	$('.grid_view_option').css('text-decoration', 'none');
 	$('.product_grid_container').hide();
 	$('.product_table_container').show();
 });
-
-// console.log($($('.product_container .product_text')[0]).parent().hide());
-
-// console.log(($('.product_container .product_text')[0].parent()));
 
 // on keypress event on input
 
@@ -111,42 +105,34 @@ $('form').keypress(function (event) {
 	return event.keyCode != 13;
 });
 
-// implement search which filters the data
+// Search which filters the data
 
-$('.search_input').on('input', function () {
-	let searchValue = $('.search_input').val().toLowerCase();
-	if (currentView === 'grid') {
-		let products = $('.product_container .product_text');
-		for (let i = 0; i < products.length; i++) {
-			let currentProduct = products[i];
-			let currentProductText = currentProduct.textContent.toLowerCase();
-			console.log(searchValue, 113);
-			console.log(currentProductText, 114);
-			if (currentProductText.indexOf(searchValue) === -1) {
-				// currentProduct.parent().hide() not working here
-				$($('.product_container .product_text')[i]).parent().hide();
-			} else {
-				$($('.product_container .product_text')[i]).parent().show();
-			}
-		}
+function filterforSearch(searchValue = '', inGridView = false) {
+	let productContainer;
+	console.log(inGridView);
+	if (!inGridView) {
+		productContainer = $('.product_table_container .product_name');
 	} else {
-		let products = $('.product_table_container .product_name');
-		for (let i = 0; i < products.length; i++) {
-			let currentProduct = products[i];
-			let currentProductText = currentProduct.textContent.toLowerCase();
-			console.log(searchValue, 113);
-			console.log(currentProductText, 114);
-			if (currentProductText.indexOf(searchValue) === -1) {
-				// currentProduct.parent().hide() not working here
-				$($('.product_table_container .product_name')[i]).parent().hide();
-			} else {
-				$($('.product_table_container .product_name')[i]).parent().show();
-			}
+		productContainer = $('.product_grid_container .product_text');
+	}
+
+	for (let i = 0; i < productContainer.length; i++) {
+		const currentProduct = productContainer[i];
+		const currentProductText = currentProduct.textContent.toLowerCase();
+		if (currentProductText.indexOf(searchValue) === -1) {
+			$(productContainer[i]).parent().hide();
+		} else {
+			$(productContainer[i]).parent().show();
 		}
 	}
+}
+
+$('.search_input').on('input', function () {
+	const searchValue = $('.search_input').val().toLowerCase();
+	filterforSearch(searchValue, inGridView);
 });
 
-$(function () {
+$(document).ready(function () {
 	$('table').tablesorter({
 		headers: {
 			'.image_head, .description_head': {
